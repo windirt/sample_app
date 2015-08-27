1deb09c4390fa918ed3ca26a6ba06b5120003b0c
Windirt Young <windirt@msn.com>
2015-08-27 20:57:31 +0800

    Add account activations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+
   def new
     @user = User.new
   end
@@ -18,9 +19,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
 
     else
       render 'new'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(name: "Windirt Young", email: "windirt@msn.com", password: "500204", password_confirmation: "500204",admin:true)
+User.create!(name: "Windirt Young", email: "windirt@msn.com", password: "500204", password_confirmation: "500204",admin:true,activated: true, activated_at: Time.zone.now)
 
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@raistutorial.org"
   password = "password"
-  User.create!(name: name, email: email, password: password, password_confirmation:password)
+  User.create!(name: name, email: email, password: password, password_confirmation:password,activated: true, activated_at: Time.zone.now)
 end
